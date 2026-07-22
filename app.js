a03a87867936d3453514a0bae262921b638a2225
George <lyakhovgm@gmail.com>
2026-07-22 13:21:01 +0300

    style: destinations slider

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   closeMenuButton.addEventListener("click", toggleMenu);
 
   setupNavigation();
+  setupSlider();
 });
 
 function toggleMenu() {
@@ -24,9 +25,9 @@ function setupNavigation() {
   const sections = [
     document.querySelector("#hero"),
     document.querySelector("#about"),
+    document.querySelector("#destinations"),
     document.querySelector("#contacts"),
   ];
-  console.log("@sections", sections);
   /** @type {NodeListOf<HTMLAnchorElement>} */
   const navLinks = document.querySelectorAll(".navigation__link");
   /** @type {IntersectionObserverInit} */
@@ -57,4 +58,122 @@ function setupNavigation() {
   }, options);
 
   sections.forEach((section) => observer.observe(section));
+}
+
+function setupSlider() {
+  /** @type {HTMLDivElement} */
+  const slider = document.querySelector(".slider");
+  if (!slider) return;
+  /** @type {HTMLButtonElement} */
+  const prevButtonMobile = document.querySelector("#prevSliderMobile");
+  /** @type {HTMLButtonElement} */
+  const nextButtonMobile = document.querySelector("#nextSliderMobile");
+  /** @type {HTMLButtonElement} */
+  const prevButton = document.querySelector("#prevSlider");
+  /** @type {HTMLButtonElement} */
+  const nextButton = document.querySelector("#nextSlider");
+  /** @type {HTMLDivElement} */
+  const container = slider.querySelector(".slider__container");
+  /** @type {NodeListOf<HTMLDivElement>} */
+  const slides = slider.querySelectorAll(".slide");
+  /** @type {HTMLDivElement} */
+  const indicatorsBlock = slider.querySelector(".slider__indicators");
+  /** @type {Array<HTMLDivElement>} */
+  const indicators = [];
+  let maxHeight = 0;
+  let slideWidth = 0;
+  let left = 0;
+  let maxLeft = 0;
+  let slideIndex = 0;
+
+  const onPrevClick = () => {
+    if (slideWidth === 0) return;
+
+    const currentLeft = parseInt(container.style.left || "0", 10) || 0;
+    slideIndex -= 1;
+    left = -1 * (slideWidth + 16) * slideIndex;
+    const isInStopPosition = left >= 0;
+
+    if (isInStopPosition) {
+      left = 0;
+    }
+
+    container.style.left = `${left}px`;
+    prevButtonMobile.style.pointerEvents = "none";
+    nextButtonMobile.style.pointerEvents = "none";
+    prevButton.style.pointerEvents = "none";
+    nextButton.style.pointerEvents = "none";
+    updateIndicators();
+  };
+  const onNextClick = () => {
+    if (slideWidth === 0) return;
+
+    const currentLeft = parseInt(container.style.left || "0", 10) || 0;
+    slideIndex += 1;
+    left = -1 * (slideWidth + 16) * slideIndex;
+
+    const isInStopPosition = Math.abs(left) >= maxLeft;
+
+    if (isInStopPosition) {
+      left = -1 * maxLeft;
+    }
+
+    container.style.left = `${left}px`;
+    prevButtonMobile.style.pointerEvents = "none";
+    nextButtonMobile.style.pointerEvents = "none";
+    prevButton.style.pointerEvents = "none";
+    nextButton.style.pointerEvents = "none";
+    updateIndicators();
+  };
+  const updateButtons = () => {
+    prevButtonMobile.disabled = left === 0;
+    nextButtonMobile.disabled = Math.abs(left) === maxLeft;
+    prevButton.disabled = left === 0;
+    nextButton.disabled = Math.abs(left) === maxLeft;
+  };
+  const updateIndicators = () => {
+    indicators.forEach((element, index) => {
+      element.classList[index === slideIndex ? "add" : "remove"]("active");
+    });
+  };
+  const onTransitionEnd = () => {
+    prevButtonMobile.style.pointerEvents = "";
+    nextButtonMobile.style.pointerEvents = "";
+    prevButton.style.pointerEvents = "";
+    nextButton.style.pointerEvents = "";
+    updateButtons();
+  };
+
+  prevButtonMobile.addEventListener("click", onPrevClick);
+  nextButtonMobile.addEventListener("click", onNextClick);
+  prevButton.addEventListener("click", onPrevClick);
+  nextButton.addEventListener("click", onNextClick);
+
+  container.addEventListener("transitionend", onTransitionEnd);
+  container.addEventListener("transitioncancel", onTransitionEnd);
+
+  slides.forEach((slide, index) => {
+    /** @type {HTMLDivElement} */
+    const specsBlock = slide.querySelector(".slide__specs");
+    if (!specsBlock) return;
+
+    const slideRect = slide.getBoundingClientRect();
+    const specsRect = specsBlock.getBoundingClientRect();
+    maxHeight = Math.max(
+      maxHeight,
+      specsRect.bottom > slideRect.bottom
+        ? specsRect.bottom - slideRect.top + 32
+        : slideRect.bottom - slideRect.top,
+    );
+    slideWidth = Math.max(slideWidth, slideRect.width);
+
+    const indicator = document.createElement("div");
+    indicator.className = "slider__indicator";
+    if (index === 0) indicator.classList.add("active");
+    indicatorsBlock.append(indicator);
+    indicators.push(indicator);
+  });
+
+  slider.style.height = `${maxHeight}px`;
+  maxLeft = container.scrollWidth - slideWidth - 32;
 }
